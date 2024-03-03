6026663a901c63d4f3651e432cd7e4a37fbaa16d
OwenTheEpicGamer <88258850+OwenTheEpicGamer@users.noreply.github.com>
2024-03-02 20:17:14 -0500

    attempt at lipsync

---
+++ backend/SynchronicityLogger.py
@@ -0,0 +1,20 @@
+import requests
+
+url = "https://api.synclabs.so/video"
+
+payload = {
+    "audioUrl": "https://drive.google.com/file/d/1FvPblbdPc0_rXliiszsqd9iQHOQjcAQi/view?usp=sharing",
+    "maxCredits": 123,
+    "model": "sync-1.5.0",
+    "synergize": True,
+    "videoUrl": "https://drive.google.com/file/d/1VhEa81-XqYv5RDehvkwVVEAggLqLN0iP/view?usp=sharing",
+    "webhookUrl": "",
+}
+headers = {
+    "x-api-key": "4e2ba9af-22ca-4c5b-9c0d-8061f3fe4213",
+    "Content-Type": "application/json",
+}
+
+response = requests.request("POST", url, json=payload, headers=headers)
+
+print(response.text)
